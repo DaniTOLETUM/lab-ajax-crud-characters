0f0a8863a63aad5be21be6653a2a93d7de938fbe
Daniel Diaz Toledano <danieldiaz.bio@gmail.com>
2019-04-15 17:41:15 +0200

    iterations 1 and 2 done with APIHandler

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,41 @@
+//esta es la lógica del API. Esto lo fabrica el que construye el API y añade los métodos que quiere para trabajar con los datos.
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios.get(this.BASE_URL + "/characters")
+      .then(response => {
+        console.log(response.request.response);
+      })
+      .catch(error => {
+        console.log("Error is: ", error);
+      })
 
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    axios.get(this.BASE_URL + "/characters" + `/${id}`)
+      .then(response => {
+        console.log('Response from the API is: ', response);
+      })
+      .catch(error => {
+        console.log("The error is: ", error);
+      });
 
   }
 
-  createOneRegister () {
+  createOneRegister() {
 
   }
 
-  updateOneRegister () {
+  updateOneRegister() {
 
   }
 
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,49 +1,60 @@
-const charactersAPI = new APIHandler("http://localhost:8000");
+//// This is the front end part. Aqui tomamos los datos del DOM para pasarlos a APIHandler, que es la lógica del API. 
 
-const theName = document.getElementsByClassName("the-name");
-const theOccupation = document.getElementsByClassName("the-occupation");
-const theCartoon = document.getElementsByClassName("the-cartoon");
-const theWeapon = document.getElementsByClassName("the-weapon");
+const charactersAPI = new APIHandler("http://localhost:8000");
 
 $(document).ready(() => {
   document.getElementById('fetch-all').onclick = function () {
     event.preventDefault();
-    console.log('fetch-all');
-    axios.get('http://localhost:8000/characters')
-      .then(response => {
-        console.log(response);
-      })
-      .catch(error => {
-        console.log("Error is: ", error);
-      })
+    charactersAPI.getFullList();
   }
 
   document.getElementById('fetch-one').onclick = function () {
     event.preventDefault();
     console.log('fetch-one');
     const theId = document.getElementById("searchID").value;
-    axios.get(`http://localhost:8000/characters/${theId}`)
+    charactersAPI.getOneRegister(theId);
+  }
+
+  document.getElementById('delete-one').onclick = function () {
+    event.preventDefault();
+    console.log('delete-one');
+    const deleteID = document.getElementById("deleteID").value;
+  }
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    event.preventDefault();
+    console.log('edit-character-form');
+    const modifyID = document.getElementById("modifyID");
+    const theNewName = document.getElementsByClassName("newName");
+    const theNewOccupation = document.getElementsByClassName("newOccupation");
+    const theNewCartoon = document.getElementsByClassName("newCartoon");
+    const theNewWeapon = document.getElementsByClassName("newWeapon");
+
+    axios.get(`http://localhost:8000/characters/${modifyID}`)
       .then(response => {
-        console.log('Response from the API is: ', response);
+        // console.log('Response from the API is: ', response.data);
+
+        // theNames is the array of all nodes that the class name 'the-name'
+        // const theNames = document.getElementsByClassName("the-name");
+
+        theNewName[1].value = response.data.name;
+        theNewOccupation[1].value = response.data.occupation;
+        theNewWeapon[1].value = response.data.weapon;
+        theNewCartoon[1].value = response.data.weapon;
+
       })
       .catch(error => {
         console.log("The error is: ", error);
       });
   }
 
-  document.getElementById('delete-one').onclick = function () {
-    event.preventDefault();
-    console.log('delete-one');
-  }
-
-  document.getElementById('edit-character-form').onsubmit = function () {
-    event.preventDefault();
-    console.log('edit-character-form');
-  }
-
   document.getElementById('new-character-form').onsubmit = function () {
     event.preventDefault();
     console.log('new-character-form');
+    const theName = document.getElementsByClassName("the-name");
+    const theOccupation = document.getElementsByClassName("the-occupation");
+    const theCartoon = document.getElementsByClassName("the-cartoon");
+    const theWeapon = document.getElementsByClassName("the-weapon");
     const characterInfo = {
       name: theName[0].value,
       occupation: theOccupation[0].value,
